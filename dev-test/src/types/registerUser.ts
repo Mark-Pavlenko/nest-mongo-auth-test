24a8658457fef074db54bd0689ca57ae6c05a64f
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2023-02-03 13:33:29 +0300

    feat: add correct regex to email validation

--- dev-test/src/types/registerUser.ts
+++ dev-test/src/types/registerUser.ts
@@ -5,7 +5,7 @@ export const registerUserSchema = joi.object({
     .example('marik8998@gmail.com')
     .max(254)
     .trim()
-    .regex(/^\s*(?:([A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})\b\s*)+$/i)
+    .regex(/^\s*(?:([A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,4})\b\s*)+$/i)
     .required()
     .email(),
   password: joi.string().example('N1fL@123').min(8).max(64).required(),
